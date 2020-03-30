0e047314ba4a3ab52f9ce0e9ca449b7fbff6926f
零捌贰壹 <15701291920@163.com>
2020-03-30 23:13:56 +0800

    属性动画  Interpolator

--- app/src/main/java/com/customview/MainActivity.java
+++ app/src/main/java/com/customview/MainActivity.java
@@ -8,6 +8,9 @@ import android.animation.ObjectAnimator;
 import android.animation.PropertyValuesHolder;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.DecelerateInterpolator;
 import android.widget.ImageView;
 
 import com.customview.animate.CircleView;
@@ -54,19 +57,30 @@ public class MainActivity extends AppCompatActivity {
 
 
         //同一个view 同一个属性 做多个动画(中间慢)
-        float length = Utils.dpTopixel(300);
-        Keyframe keyframe1 = Keyframe.ofFloat(0, 0);
-        Keyframe keyframe2 = Keyframe.ofFloat(0.2f, 0.4f * length);
-//        Keyframe keyframe2 = Keyframe.ofFloat(0.2f, 1.5f * length);//超出会回弹
-        Keyframe keyframe3 = Keyframe.ofFloat(0.8f, 0.6f * length);
-        Keyframe keyframe4 = Keyframe.ofFloat(1, 1 * length);
-        PropertyValuesHolder propertyValuesHolder = PropertyValuesHolder.ofKeyframe("translationX",
-                keyframe1, keyframe2, keyframe3, keyframe4);
-        ObjectAnimator objectAnimator = ObjectAnimator.ofPropertyValuesHolder(view, propertyValuesHolder);
-        objectAnimator.setStartDelay(1000);
-        objectAnimator.setDuration(2000);
-        objectAnimator.start();
+//        float length = Utils.dpTopixel(300);
+//        Keyframe keyframe1 = Keyframe.ofFloat(0, 0);
+//        Keyframe keyframe2 = Keyframe.ofFloat(0.2f, 0.4f * length);
+////        Keyframe keyframe2 = Keyframe.ofFloat(0.2f, 1.5f * length);//超出会回弹
+//        Keyframe keyframe3 = Keyframe.ofFloat(0.8f, 0.6f * length);
+//        Keyframe keyframe4 = Keyframe.ofFloat(1, 1 * length);
+//        PropertyValuesHolder propertyValuesHolder = PropertyValuesHolder.ofKeyframe("translationX",
+//                keyframe1, keyframe2, keyframe3, keyframe4);
+//        ObjectAnimator objectAnimator = ObjectAnimator.ofPropertyValuesHolder(view, propertyValuesHolder)se;
+//        objectAnimator.setStartDelay(1000);
+//        objectAnimator.setDuration(2000);
+//        objectAnimator.start();
 
+
+        // interpolator 差值器  默认 先加速后减速过程
+
+        view.setTranslationY(-Utils.dpTopixel(100));
+        view.animate()
+                .translationY(Utils.dpTopixel(300))
+                .setStartDelay(1000)
+                .setDuration(2000)
+                .setInterpolator(new DecelerateInterpolator())// 减速的差值器
+//                .setInterpolator(new AccelerateInterpolator())// 加速的差值器
+                .start();
 
     }
 }
