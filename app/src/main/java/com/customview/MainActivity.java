f2e2fbc9bf2f380eabd87d80096b0e3e038722bb
零捌贰壹 <15701291920@163.com>
2020-03-30 22:12:45 +0800

    属性动画

--- app/src/main/java/com/customview/MainActivity.java
+++ app/src/main/java/com/customview/MainActivity.java
@@ -2,13 +2,26 @@ package com.customview;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
+import android.view.View;
+
+import com.customview.animate.CircleView;
 
 public class MainActivity extends AppCompatActivity {
+
+    CircleView view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        view = findViewById(R.id.view);
+
+        ObjectAnimator animator = ObjectAnimator.ofFloat(view, "radius", Utils.dp2px(150));
+        animator.setStartDelay(1000);
+        animator.start();
+
     }
 }
